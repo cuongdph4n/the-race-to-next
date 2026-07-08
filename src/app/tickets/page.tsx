affb972d4c083f0ed4e1f96fb3e858ceb229fe88
Cuong <cuongdph4n@gmail.com>
2026-07-08 23:10:41 +0700

    Link Components

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,5 +1,20 @@
-const HomePage = () => {
-  return <h2 className="text-lg">TicketsPage</h2>;
+import Link from "next/link";
+import { initialTickets } from "../../data";
+
+const TicketsPage = () => {
+  return (
+    <div>
+      {initialTickets.map((ticket) => (
+        <div key={ticket.id}>
+          <h2 className="text-lg">{ticket.title}</h2>
+
+          <Link href={`/tickets/${ticket.id}`} className="text-sm underline">
+            View
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
 };
 
-export default HomePage;
+export default TicketsPage;
