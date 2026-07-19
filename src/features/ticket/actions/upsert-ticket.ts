fd5ff17be931622ae55edafad4d9db25633075e5
Cuong <cuongdph4n@gmail.com>
2026-07-19 22:36:28 +0700

    Error Handling

--- src/features/ticket/actions/upsert-ticket.ts
+++ src/features/ticket/actions/upsert-ticket.ts
@@ -1,5 +1,9 @@
 "use server";
 
+import {
+  ActionState,
+  fromErrorToActionState,
+} from "@/components/form/utils/to-action-state";
 import { prisma } from "@/lib/prisma";
 import { ticketPath, ticketsPath } from "@/paths";
 import { revalidatePath } from "next/cache";
@@ -13,10 +17,7 @@ const upsertTicketSchema = z.object({
 
 export const upsertTicket = async (
   id: string | undefined,
-  _actionState: {
-    message: string;
-    payload?: FormData;
-  },
+  _actionState: ActionState,
   formData: FormData,
 ) => {
   try {
@@ -33,10 +34,7 @@ export const upsertTicket = async (
       create: data,
     });
   } catch (error) {
-    return {
-      message: "Something went wrong",
-      payload: formData,
-    };
+    return fromErrorToActionState(error, formData);
   }
 
   revalidatePath(ticketsPath());
