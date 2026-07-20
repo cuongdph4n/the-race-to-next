10da59e84c72639de7df41f01b32a9328ea7194a
Cuong <cuongdph4n@gmail.com>
2026-07-20 16:07:28 +0700

    Premature Optimization

--- src/components/form/utils/to-action-state.ts
+++ src/components/form/utils/to-action-state.ts
@@ -1,9 +1,17 @@
 import * as z from "zod";
 
 export type ActionState = {
+  status?: "SUCCESS" | "ERROR";
   message: string;
   payload?: FormData;
   fieldErrors: Record<string, string[] | undefined>;
+  timestamp: number;
+};
+
+export const EMPTY_ACTION_STATE: ActionState = {
+  message: "",
+  fieldErrors: {},
+  timestamp: Date.now(),
 };
 
 export const fromErrorToActionState = (
@@ -14,25 +22,43 @@ export const fromErrorToActionState = (
     // if validation error with Zod, return first error message
     console.log("error", error);
     return {
+      status: "ERROR",
       message: "",
       payload: formData,
       fieldErrors: z.flattenError(error).fieldErrors,
+      timestamp: Date.now(),
     };
   } else if (error instanceof Error) {
     // if another error instance, return error message
     // e.g. database error
     return {
+      status: "ERROR",
       message: error.message,
       payload: formData,
       fieldErrors: {},
+      timestamp: Date.now(),
     };
   } else {
     // if not an error instance but something else crashed
     // return generic error message
     return {
+      status: "ERROR",
       message: "An unknown error occurred",
       payload: formData,
       fieldErrors: {},
+      timestamp: Date.now(),
     };
   }
+};
+
+export const toActionState = (
+  status: ActionState["status"],
+  message: string,
+): ActionState => {
+  return {
+    status,
+    message,
+    fieldErrors: {},
+    timestamp: Date.now(),
+  };
 };
