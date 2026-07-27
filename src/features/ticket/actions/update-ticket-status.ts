995f4b34b640b6bb60f5c7a6686e2a6cc23dc125
Cuong <cuongdph4n@gmail.com>
2026-07-27 15:02:10 +0700

    Ticket Status

---
+++ src/features/ticket/actions/update-ticket-status.ts
@@ -0,0 +1,29 @@
+"use server";
+
+import {
+  fromErrorToActionState,
+  toActionState,
+} from "@/components/form/utils/to-action-state";
+import { TicketStatus } from "@/generated/prisma/enums";
+import { prisma } from "@/lib/prisma";
+import { ticketsPath } from "@/paths";
+import { revalidatePath } from "next/cache";
+
+export const updateTicketStatus = async (id: string, status: TicketStatus) => {
+  try {
+    await prisma.ticket.update({
+      where: {
+        id,
+      },
+      data: {
+        status,
+      },
+    });
+  } catch (error) {
+    return fromErrorToActionState(error);
+  }
+
+  revalidatePath(ticketsPath());
+
+  return toActionState("SUCCESS", "Status updated");
+};
--- src/features/ticket/components/ticket-more-menu.tsx
+++ src/features/ticket/components/ticket-more-menu.tsx
@@ -3,12 +3,17 @@
 import {
   DropdownMenu,
   DropdownMenuContent,
-  DropdownMenuGroup,
   DropdownMenuItem,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { Ticket } from "@/generated/prisma/client";
+import { Ticket, TicketStatus } from "@/generated/prisma/client";
 import { LucideTrash } from "lucide-react";
+import { toast } from "sonner";
+import { updateTicketStatus } from "../actions/update-ticket-status";
+import { TICKET_STATUS_LABELS } from "../constants";
 
 type TicketMoreMenuProps = {
   ticket: Ticket;
@@ -17,17 +22,42 @@ type TicketMoreMenuProps = {
 
 const TicketMoreMenu = ({ ticket, trigger }: TicketMoreMenuProps) => {
   const deleteButton = (
-    <DropdownMenuItem>
+    <DropdownMenuItem variant="destructive">
       <LucideTrash className="h-4 w-4" />
       <span>Delete</span>
     </DropdownMenuItem>
+  );
+
+  const handleUpdateTicketStatus = async (value: string) => {
+    const result = await updateTicketStatus(ticket.id, value as TicketStatus);
+
+    if (result.status === "ERROR") {
+      toast.error(result.message);
+    } else if (result.status === "SUCCESS") {
+      toast.success(result.message);
+    }
+  };
+
+  const ticketStatusRadioGroupItems = (
+    <DropdownMenuRadioGroup
+      value={ticket.status}
+      onValueChange={handleUpdateTicketStatus}
+    >
+      {(Object.keys(TICKET_STATUS_LABELS) as Array<TicketStatus>).map((key) => (
+        <DropdownMenuRadioItem key={key} value={key}>
+          {TICKET_STATUS_LABELS[key]}
+        </DropdownMenuRadioItem>
+      ))}
+    </DropdownMenuRadioGroup>
   );
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger render={trigger} />
       <DropdownMenuContent className="w-56" side="right">
-        <DropdownMenuGroup>{deleteButton}</DropdownMenuGroup>
+        {ticketStatusRadioGroupItems}
+        <DropdownMenuSeparator />
+        {deleteButton}
       </DropdownMenuContent>
     </DropdownMenu>
   );
